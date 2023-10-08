7cceda16b2fb7f110bb500adf2e43103e9dd45be
magonazz1 <martinmagonazz@outlook.com>
2023-10-07 19:38:53 -0700

    hot fix

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 for i in range(0, 100):
-    print("{}".format(i))
-    print(end=", ")
+    if (i == 99):
+        print("{}".format(i))
+    else:
+        print("{:02}".format(i), end=", ")
