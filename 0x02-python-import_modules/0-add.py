47afb9533b0ebf8caabbb38bf61ba839749a1491
magonazz1 <martinmagonazz@outlook.com>
2023-10-10 16:07:56 -0700

    mandatory tasks

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -3,7 +3,7 @@
 if __name__ == "__main__":
     from add_0 import add
 
-    a = int(1)
-    b = int(2)
+    a = 1
+    b = 2
 
     print("{} + {} = {}".format(a, b, add(a, b)))
--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    from calculator_1 import (add, sub, mul, div)
+    from calculator_1 import add, sub, mul, div
 
-    a = int(10)
-    b = int(5)
+    a = 10
+    b = 5
 
     print("{} + {} = {}".format(a, b, add(a, b)))
     print("{} - {} = {}".format(a, b, sub(a, b)))
