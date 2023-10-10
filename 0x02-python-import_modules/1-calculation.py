db882f8f76b027eb04e2897e0831d82649bebcad
magonazz1 <martinmagonazz@outlook.com>
2023-10-10 16:04:55 -0700

    mandatory tasks

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -3,10 +3,10 @@
 if __name__ == "__main__":
     from calculator_1 import (add, sub, mul, div)
 
-a = int(10)
-b = int(5)
+    a = int(10)
+    b = int(5)
 
-print("{} + {} = {}".format(a, b, add(a, b)))
-print("{} - {} = {}".format(a, b, sub(a, b)))
-print("{} * {} = {}".format(a, b, mul(a, b)))
-print("{} / {} = {}".format(a, b, div(a, b)))
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
