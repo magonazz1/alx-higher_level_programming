2a8e8ba8aaae83255434b6460e245e84bddc2438
magonazz1 <martinmagonazz@outlook.com>
2023-10-11 14:31:25 -0700

    uplink

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,8 +3,7 @@
 def print_matrix_integer(matrix=[[]]):
     for x in range(len(matrix)):
         for y in range(len(matrix[x])):
-                print("{:d}".format(matrix[x][y]), end="")
-                if y != (len(matrix[x]) - 1):
-                    print(" ", end="")
-
+            print("{:d}".format(matrix[x][y]), end="")
+            if y != (len(matrix[x]) - 1):
+                print(" ", end="")
         print("")
