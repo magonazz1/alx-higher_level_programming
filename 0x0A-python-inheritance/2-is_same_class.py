e16ae687858788b187ddea74a839d47dffdabfdd
magonazz1 <martinmagonazz@outlook.com>
2023-11-11 01:57:34 -0800

    file-2 fixup

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -4,6 +4,15 @@ Write a function that returns True if the object is exactly
 an instance of the specified class; otherwise False.
 '''
 
-
 def is_same_class(obj, a_class):
-    return issubclass(a_class, type(obj))
+    return isinstance(obj, a_class)
+
+# Example usage:
+class ExampleClass:
+    pass
+
+obj1 = ExampleClass()
+obj2 = "This is a string"
+
+print(is_same_class(obj1, ExampleClass))  # True
+print(is_same_class(obj2, ExampleClass))  # False
